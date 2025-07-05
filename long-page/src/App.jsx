f236b9fc73402fc9f662f8064e4219723fc15c6d
anandavallis <sanandavalli7@gmail.com>
2025-07-05 15:21:50 +0530

    Create App.jsx

---
+++ long-page/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import ScrollToSection from './components/ScrollToSection';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Scroll to Element Example</h1>
+      <ScrollToSection />
+    </div>
+  );
+};
+
+export default App;
